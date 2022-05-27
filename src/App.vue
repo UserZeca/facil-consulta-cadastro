7223d4241a8f711c8903ea2e0859c703fa1cc670
Ezequias Kluyvert de Oliveira Lemos <61207416+UserZeca@users.noreply.github.com>
2022-05-27 16:02:49 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CardBody src="https://res.cloudinary.com/dyie6o63u/image/upload/v1653663257/desktop-pagina-1_1_dvtzjh.png" backgroundColor=""/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CardBody from './components/CardBody.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CardBody
+  },
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CardBody.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="card mb-3" style="max-width: 80%; max-height: 80%;" >
+      <div class="row g-0">
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <img :src="src" class="img-fluid rounded-start" alt="...">
+        </div>
+      </div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+
+    </div>
+    
+</template>
+
+<script>
+
+import HelloWorld from './HelloWorld.vue'
+
+
+export default {
+  name: 'CardBody',
+  components: {
+    HelloWorld
+  },  
+  props: {
+        src: {
+            type: String,
+            default: '',
+        },
+    },
+    
+}
+
+
+</script>
+
+<style scoped>
+
+
+</style>
